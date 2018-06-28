1eaa954dea59cb2dc02e03936c86de77e853a943
doubleknd26 <doubleknd26@gmail.com>
2018-06-28 23:53:34 +0900

    [thread] add scheduled executor service test

--- thread/src/test/java/com/doubleknd26/exercise/thread/ExecutorTest.java
+++ thread/src/test/java/com/doubleknd26/exercise/thread/ExecutorTest.java
@@ -2,9 +2,7 @@ package com.doubleknd26.exercise.thread;
 
 import org.junit.Test;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 import static org.junit.Assert.*;
 
@@ -39,5 +37,28 @@ public class ExecutorTest {
         service.submit(new RunnableTask(3));
 
         service.shutdown();
+    }
+
+    @Test
+    public void testScheduledExecutorService() {
+        // count means how many time we want to run scheduled executor.
+        long count = 2;
+        long period = 1000;
+        long initialDelay = 0;
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        scheduler.scheduleAtFixedRate(
+                new RunnableTask(1)
+                , initialDelay
+                , period
+                , TimeUnit.MILLISECONDS);
+        try {
+            // scheduled executor will be executed by count.
+            Thread.sleep(count * period);
+            scheduler.shutdown();
+            scheduler.awaitTermination(1000, TimeUnit.MILLISECONDS);
+            System.out.println("scheduler is shutdown");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
