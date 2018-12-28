7306aac5d296649ebd5fa8a77350934821a3b77a
doubleknd26 <doubleknd26@gmail.com>
2018-12-28 18:21:22 +0900

    test springboot ehcache

---
+++ springboot/src/main/java/com/doubleknd26/exercise/springboot/model/Member.java
@@ -0,0 +1,30 @@
+package com.doubleknd26.exercise.springboot.model;
+
+/**
+ * Created by doubleknd26 on 2018-12-28.
+ */
+public class Member {
+    private String name;
+    private int age;
+
+    public Member(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ springboot/src/test/java/com/doubleknd26/exercise/springboot/MemberApplicationTest.java
@@ -0,0 +1,19 @@
+package com.doubleknd26.exercise.springboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by doubleknd26 on 2018-12-28.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberApplicationTest {
+
+    @Test
+    public void contextLoad() {
+
+    }
+}
