1686abb106a21f0e15ca3497e9980d40d2ea49b0
doubleknd26 <doubleknd26@gmail.com>
2019-08-29 17:26:50 +0900

    [algorithm] baekjoon online judge

--- algorithm/src/main/java/com/doubleknd26/exercise/algorithm/baekjoon/_1786.java
+++ algorithm/src/main/java/com/doubleknd26/exercise/algorithm/baekjoon/_1786.java
@@ -11,29 +11,30 @@ public class _1786 {
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    public static void main (String[] args) throws IOException {
-        char[] t = br.readLine().toCharArray();
-        char[] p = br.readLine().toCharArray();
-
-        int[] pi = getPi(p);
-        List<Integer> ans = doKmpSearch(t, p, pi);
-
-        bw.write(ans.size() + "\n");
-        for (int a : ans) {
-            bw.write(a + "\n");
-        }
+    public static void main(String[] args) throws IOException {
+        start();
         bw.flush();
         bw.close();
         br.close();
     }
 
+    private static void start() throws IOException {
+        char[] t = br.readLine().toCharArray();
+        char[] p = br.readLine().toCharArray();
+        int[] pi = getPi(p);
+        List<Integer> ans = runKmp(t, p, pi);
+        bw.write(ans.size() + "\n");
+        for (Integer loc : ans) {
+            bw.write(loc + "\n");
+        }
+    }
+
     private static int[] getPi(char[] p) {
-        int len = p.length;
-        int[] pi = new int[len];
-        int j = 0;
-        for (int i = 1 ; i < len ; i++) {
-            while (j > 0 && p[i] != p[j]) {
-                j = pi[j - 1];
+        int[] pi = new int[p.length];
+        int j=0;
+        for (int i=1 ; i<p.length ; i++) {
+            while (j>0 && p[i] != p[j]) {
+                j = pi[j-1];
             }
             if (p[i] == p[j]) {
                 pi[i] = ++j;
@@ -42,17 +43,15 @@ public class _1786 {
         return pi;
     }
 
-    private static List<Integer> doKmpSearch(char[] t, char[] p, int[] pi) {
-        int tlen = t.length;
-        int plen = p.length;
+    private static List<Integer> runKmp(char[] t, char[] p, int[] pi) {
         List<Integer> ans = new ArrayList<>();
-        int j = 0;
-        for (int i = 0; i < tlen ; i++) {
-            while (j > 0 && t[i] != p[j]) {
-                j = pi[j - 1];
+        int j=0;
+        for (int i=0 ; i<t.length ; i++) {
+            while (j>0 && t[i] != p[j]) {
+                j = pi[j-1];
             }
             if (t[i] == p[j]) {
-                if (j == plen - 1) {
+                if (p.length == j + 1) {
                     ans.add(i - j + 1);
                     j = pi[j];
                 } else {
