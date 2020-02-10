d8dac9a82f5870ebfc47917840a582f5309c4fa2
doubleknd26 <doubleknd26@gmail.com>
2020-02-10 23:09:40 +0900

    [algorithm] divide and conquer

---
+++ algorithm/src/main/java/com/doubleknd26/exercise/algorithm/baekjoon/divideandconquer/_10830.java
@@ -0,0 +1,26 @@
+package com.doubleknd26.exercise.algorithm.baekjoon.divideandconquer;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/**
+ * https://www.acmicpc.net/problem/10830
+ */
+public class _10830 {
+	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+	public static void main (String[] args) throws Exception {
+		String[] input = br.readLine().split(" ");
+		int n = Integer.parseInt(input[0]);
+		long b = Long.parseLong(input[1]);
+
+
+
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
