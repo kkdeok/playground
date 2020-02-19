01ca977805b0666d6720eddf0784e21ac1eb53f6
doubleknd26 <doubleknd26@gmail.com>
2020-02-19 23:11:07 +0900

    [algorithm] graph

--- algorithm/src/main/java/com/doubleknd26/exercise/algorithm/baekjoon/graph/_2617.java
+++ algorithm/src/main/java/com/doubleknd26/exercise/algorithm/baekjoon/graph/_2617.java
@@ -5,6 +5,7 @@ import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -14,39 +15,37 @@ public class _2617 {
 	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-	private static List<List<Integer>> adj = new ArrayList<>();
 	private static int[] visit = new int[100];
-	private static int[][] comp = new int[100][2];
 
 	public static void main (String[] args) throws Exception {
 		String[] input = br.readLine().split( " ");
 		int n = Integer.parseInt(input[0]);
 		int m = Integer.parseInt(input[1]);
 
+		List<List<Integer>> bigAdj = new ArrayList<>();
+		List<List<Integer>> smlAdj = new ArrayList<>();
+
+		// init
 		for (int i=0 ; i<=n ; i++) {
-			adj.add(new ArrayList<>());
+			bigAdj.add(new ArrayList<>());
+			smlAdj.add(new ArrayList<>());
 		}
 
 		for (int i=0 ; i<m ; i++) {
 			input = br.readLine().split(" ");
-			int a = Integer.parseInt(input[0]);
-			int b = Integer.parseInt(input[1]);
-			adj.get(b).add(a);
+			int x = Integer.parseInt(input[0]);
+			int y = Integer.parseInt(input[1]);
+			bigAdj.get(y).add(x);
+			smlAdj.get(x).add(y);
 		}
 
-		for (int i=1 ; i<=n ; i++) {
-			if (visit[i] == 0) {
-				comp[i][0] += 0;
-				comp[i][1] += dfs(i, 0);
-			}
-		}
-
-		int cnt = n * (n-1) / 2;
 		int ans = 0;
+		int mid = (n+1) / 2;
 		for (int i=1 ; i<=n ; i++) {
-			System.out.println(comp[i][0] + " : " + comp[i][1]);
-
-			if (comp[i][0] >= cnt || comp[i][1] >= cnt) {
+			// 특정 숫자 (i)를 기준으로 i 보다 큰 것을 찾을 때와 작은 것을 찾을 때, 방문하는 위치가
+			// 절대 겹칠 수 없기 때문에 visit 초기화를 각가 해줄 필요가 없이, 여기서 한번만 해주면 된다.
+			Arrays.fill(visit, 0);
+			if (dfs(i, bigAdj) >= mid || dfs(i, smlAdj) >= mid) {
 				ans++;
 			}
 		}
@@ -56,23 +55,15 @@ public class _2617 {
 		br.close();
 	}
 
-	private static int dfs(int point, int smallCount) {
-		visit[point] = 1;
-		List<Integer> list = adj.get(point);
-
-		int biggerCount = 0;
-		int size = list.size();
-		for (int i=0 ; i<size ; i++) {
-			int tmp = list.get(i);
-			if (visit[tmp] == 0) {
-				biggerCount += dfs(tmp, smallCount + 1) + 1;
-			} else {
-				comp[tmp][0] += smallCount;
-				biggerCount += comp[tmp][1];
+	private static int dfs (int idx, List<List<Integer>> adj) {
+		visit[idx] = 1;
+		int cnt = 0;
+		List<Integer> list = adj.get(idx);
+		for (int next : list) {
+			if (visit[next] == 0) {
+				cnt += dfs(next, adj) + 1;
 			}
 		}
-		comp[point][0] += smallCount;
-		comp[point][1] += biggerCount;
-		return comp[point][1];
+		return cnt;
 	}
 }
