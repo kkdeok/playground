91cbf18fc66e61c0cbaa32b26ecfcc9a3493b391
doubleknd26 <doubleknd26@gmail.com>
2020-03-23 14:27:15 +0900

    [macro] update docker run script

--- macro/docker_run.sh
+++ macro/docker_run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 docker build --no-cache -t macro .
-docker run -p 4444:4444 --shm-size=2g --name macro_app macro
+docker run -p 4444:4444 --shm-size=2g -v /tmp/logs:/tmp/logs --name macro_app macro
