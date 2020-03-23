92baa48ec6ca52f47f9d06131ddd0b0e9da2d7c3
doubleknd26 <doubleknd26@gmail.com>
2020-03-23 14:27:46 +0900

    [macro] remove unused code

--- macro/src/main/java/com/doubleknd26/exercise/macro/mask/search/CoupangSearcher.java
+++ macro/src/main/java/com/doubleknd26/exercise/macro/mask/search/CoupangSearcher.java
@@ -18,8 +18,6 @@ public class CoupangSearcher extends Searcher {
 
 	@Override
 	void login() {
-		logger.info("login: " + driver.driver.getCurrentUrl());
-		logger.info(driver.driver.getPageSource());
 		WebElement loginBtn = driver.findClickableElement(By.id("login"));
 		driver.clickAndWait(loginBtn);
 		driver.sendKeyToElement(By.className("_loginIdInput"), targetInfo.getId());
