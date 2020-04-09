13c2becede23deb3f7c491a432ea1e27664fed37
doubleknd26 <doubleknd26@gmail.com>
2020-04-09 16:23:22 +0900

    [algorithm] graph

--- algorithm/src/main/java/com/doubleknd26/exercise/algorithm/baekjoon/graph/_1260.java
+++ algorithm/src/main/java/com/doubleknd26/exercise/algorithm/baekjoon/graph/_1260.java
@@ -1,9 +1,6 @@
 package com.doubleknd26.exercise.algorithm.baekjoon.graph;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
@@ -13,63 +10,58 @@ import java.util.Queue;
  * https://www.acmicpc.net/problem/1260
  */
 public class _1260 {
-	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 	private static final int SIZE = 1001;
-	private static int[][] graph = new int[SIZE][SIZE];
-	private static int[] check = new int[SIZE];
+	private static final int[][] board = new int[SIZE][SIZE];
+	private static boolean[] check = new boolean[SIZE];
 
-
-	public static void main (String[] args) throws Exception {
+	private static void start() throws IOException {
 		String[] input = br.readLine().split(" ");
 		int n = Integer.parseInt(input[0]);
 		int m = Integer.parseInt(input[1]);
 		int v = Integer.parseInt(input[2]);
-
-		for (int i=0 ; i<m ; i++) {
-			String[] line = br.readLine().split(" ");
-			int x = Integer.parseInt(line[0]);
-			int y = Integer.parseInt(line[1]);
-			graph[x][y] = graph[y][x] = 1; // there is an edge.
+		for (int i=0; i<m ; i++) {
+			input = br.readLine().split(" ");
+			int x = Integer.parseInt(input[0]);
+			int y = Integer.parseInt(input[1]);
+			board[x][y] = board[y][x] = 1;
 		}
-
-		Arrays.fill(check, 0);
-		dfs(n, v);
-		bw.write("\n");
-
-		Arrays.fill(check, 0);
-		bfs(n, v);
-		bw.write("\n");
-
-		bw.flush();
-		bw.close();
-		br.close();
+		doDFS(v, n);
+		Arrays.fill(check, false);
+		System.out.println();
+		doBFS(v, n);
 	}
 
-	private static void dfs(int n, int x) throws Exception {
-		check[x] = 1;
-		bw.write(x + " ");
-		for (int y=1 ; y<=n ; y++) {
-			if (graph[x][y] == 1 && check[y] == 0) {
-				dfs(n, y);
+	private static void doDFS(int v, int n) {
+		check[v] = true;
+		System.out.print(v + " ");
+		for (int i=1 ; i<=n ; i++) {
+			if (board[v][i] == 1 && !check[i]) {
+				doDFS(i, n);
 			}
 		}
 	}
 
-	private static void bfs(int n, int x) throws Exception {
+	private static void doBFS(int v, int n) {
 		Queue<Integer> q = new LinkedList<>();
-		q.add(x);
-		check[x] = 1;
+		q.add(v);
+		check[v] = true;
+
 		while (!q.isEmpty()) {
-			int tmpX = q.poll();
-			bw.write(tmpX + " ");
-			for (int y=1 ; y<=n ; y++) {
-				if (graph[tmpX][y] == 1 && check[y] == 0) {
-					q.add(y);
-					check[y] = 1;
+			int tmpv = q.poll();
+			System.out.print(tmpv + " ");
+			for (int i=1 ; i<=n ; i++) {
+				if (board[tmpv][i] == 1 && !check[i]) {
+					q.add(i);
+					check[i] = true;
 				}
 			}
 		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		start();
+		br.close();
 	}
 }
