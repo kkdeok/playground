3890641351e61f20c1f0da8d34cd7c38cff6b58b
doubleknd26 <doubleknd26@gmail.com>
2020-04-12 12:08:58 +0900

    [lucene] synonym analyzer

--- lucene/src/main/java/com/doubleknd26/exercise/lucene/analyzer/SynonymAnalyzer.java
+++ lucene/src/main/java/com/doubleknd26/exercise/lucene/analyzer/SynonymAnalyzer.java
@@ -1,5 +1,6 @@
 package com.doubleknd26.exercise.lucene.analyzer;
 
+import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.core.LowerCaseFilter;
 import org.apache.lucene.analysis.core.StopAnalyzer;
@@ -7,19 +8,23 @@ import org.apache.lucene.analysis.core.StopFilter;
 import org.apache.lucene.analysis.standard.StandardFilter;
 import org.apache.lucene.analysis.standard.StandardTokenizer;
 
-import java.io.Reader;
 
-public class SynonymAnalyzer {
+public class SynonymAnalyzer extends Analyzer {
 	private SynonymEngine engine;
 
 	public SynonymAnalyzer(SynonymEngine engine) {
 		this.engine = engine;
 	}
 
-	public TokenStream tokenStream(String fieldName, Reader reader) {
-		final StandardTokenizer st = new StandardTokenizer();
-		st.setReader(reader);
-		final TokenStream result = new SynonymFilter(new StopFilter(new LowerCaseFilter(new StandardFilter(st)), StopAnalyzer.ENGLISH_STOP_WORDS_SET), engine);
-		return result;
+	@Override
+	protected TokenStreamComponents createComponents(String fieldName) {
+		StandardTokenizer tokenizer = new StandardTokenizer();
+		TokenStream tokenStream  = new SynonymFilter(
+				new StopFilter(
+						new LowerCaseFilter(new StandardFilter(tokenizer))
+						, StopAnalyzer.ENGLISH_STOP_WORDS_SET)
+				, engine);
+		TokenStreamComponents tsc = new TokenStreamComponents(tokenizer, tokenStream);
+		return tsc;
 	}
 }
