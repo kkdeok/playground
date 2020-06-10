ace5ed1793f643cbf323684eaf3f6efb1e03eb6e
doubleknd26 <doubleknd26@gmail.com>
2020-06-10 12:50:03 +0900

    [macro] refactoring about docker build

--- macro/docker/docker_run.sh
+++ macro/docker/docker_run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 PASSPHARSE=$1
-sudo docker build --no-cache --build-arg passphrase=${PASSPHARSE} -t macro .
+sudo docker build --build-arg passphrase=${PASSPHARSE} -t macro .
 sudo docker run -d -p 4444:4444 -p 11619:11619 --shm-size=128m -v /tmp/logs:/tmp/logs --name macro_app macro
