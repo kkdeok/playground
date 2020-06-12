7d434ee3c8ffc093bba64bf031b8f81db921e877
doubleknd26 <doubleknd26@gmail.com>
2020-06-12 14:44:52 +0900

    [macro] refactoring

--- macro/src/main/java/com/doubleknd26/exercise/macro/MacroTypeConverter.java
+++ macro/src/main/java/com/doubleknd26/exercise/macro/MacroTypeConverter.java
@@ -4,13 +4,20 @@ import com.beust.jcommander.IStringConverter;
 import com.beust.jcommander.ParameterException;
 import com.doubleknd26.exercise.macro.util.MacroType;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class MacroTypeConverter implements IStringConverter<MacroType> {
 	@Override
 	public MacroType convert(String value) {
 		MacroType macroType = MacroType.fromString(value);
 		if(macroType == null) {
-			throw new ParameterException(String.format("Value %s cannot be converted to MacroType."
-					+ " Available values are: MASK.", value));
+			throw new ParameterException(
+					String.format("Value %s cannot be converted to MacroType. Available values are: %s.", 
+							value, 
+							Arrays.stream(MacroType.values())
+									.map(MacroType::getName)
+									.collect(Collectors.joining(","))));
 		}
 		return macroType;
 	}
