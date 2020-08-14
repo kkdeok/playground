4c7e28f7de4364cc749f0c5cc908a079b5524618
doubleknd26 <doubleknd26@gmail.com>
2020-08-14 16:00:55 +0900

    algorithm

--- algorithm/src/main/java/com/kkd/exercise/algorithm/baekjoon/dynamic/_2579.java
+++ algorithm/src/main/java/com/kkd/exercise/algorithm/baekjoon/dynamic/_2579.java
@@ -1,41 +1,40 @@
 package com.kkd.exercise.algorithm.baekjoon.dynamic;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
 
 /**
  * https://www.acmicpc.net/problem/2579
  */
 public class _2579 {
-	private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-	private static final int SIZE = 301;
-	private static int[] stairs = new int[SIZE];
-	private static int[] memo = new int[SIZE];
-
-	public static void main(String[] args) throws Exception {
-		int n = Integer.parseInt(br.readLine());
-		for (int i=1 ; i<=n ; i++) {
-			stairs[i] = Integer.parseInt(br.readLine());
-		}
-		Arrays.fill(memo, -1);
-		bw.write(find(n) + "\n");
-		bw.flush();
-		bw.close();
-		br.close();
-	}
-
-	private static int find(int n) {
-		if (n <= 0) {
-			return 0;
-		}
-		if (memo[n] != -1) {
-			return memo[n];
-		}
-		return memo[n] = stairs[n] + Math.max(find(n-2), stairs[n-1] + find(n-3));
-	}
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    private static int[] arr = new int[300];
+    // memo[n][0] = 1 step
+    // memo[n][1] = 2 step
+    private static int[][] memo = new int[300][2];
+    
+    public static void main(String[] args) throws Exception {
+        int n = Integer.parseInt(br.readLine());
+        for (int i=0 ; i<n ; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        int ans = Math.max(doDP(n-1, 0), doDP(n-1, 1));
+        System.out.println(ans);
+    }
+    
+    private static int doDP(int n, int i) {
+        if (n < 0) {
+            return 0;
+        }
+        if (memo[n][i] != 0) {
+            return memo[n][i];
+        }
+        
+        if (i == 0) {
+            return memo[n][i] = arr[n] + doDP(n - 2, 1);
+        } else {
+            return memo[n][i] = arr[n] + Math.max(doDP(n-1, 0), doDP(n-2, 1));
+        }
+    }
 }
