d07f19e59ec0615423f152ffebce730fd1979cf6
doubleknd26 <doubleknd26@gmail.com>
2020-08-15 16:56:09 +0900

    algorithm

---
+++ java/src/test/java/com/kkd/exercise/java/ListTest.java
@@ -0,0 +1,31 @@
+package com.kkd.exercise.java;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ListTest {
+    
+    @Test
+    public void deepCopy() {
+        List<Integer> oldList = Arrays.asList(1, 2, 3, 4, 5);
+        List<Integer> newList = new ArrayList<>(oldList);
+        
+        Assert.assertEquals(oldList, newList);
+        Assert.assertNotSame(oldList, newList); 
+    }
+    
+    @Test
+    public void deepCopyAndChange() {
+        List<Integer> oldList = Arrays.asList(1, 2, 3, 4, 5);
+        List<Integer> newList = new ArrayList<>(oldList);
+        
+        newList.add(6);
+        
+        Assert.assertNotEquals(oldList, newList);
+        Assert.assertNotSame(oldList, newList);
+    }
+}
