9567ec17e6b360c970d1431cef6ee2e0c92833ab
doubleknd26 <doubleknd26@gmail.com>
2020-08-18 17:07:48 +0900

    [java] static method in interface

--- java/src/main/java/com/kkd/exercise/java/interfaces/Interface1.java
+++ java/src/main/java/com/kkd/exercise/java/interfaces/Interface1.java
@@ -1,12 +1,14 @@
 package com.kkd.exercise.java.interfaces;
 
-import java.util.HashMap;
 
 public interface Interface1 {
 	void one();
 	
 	default void printInterface1() {
-		HashMap
 		System.out.println("interface1");
 	}
+	
+	static void printStaticInterface1() {
+		System.out.println("static interface1");
+	} 
 }
