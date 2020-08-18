cfccc945cbbc0acd60baa4b10acbc2e34bbf4890
doubleknd26 <doubleknd26@gmail.com>
2020-08-18 17:12:13 +0900

    [java] abstract class

---
+++ java/src/main/java/com/kkd/exercise/java/abstracts/AbstractClass.java
@@ -0,0 +1,14 @@
+package com.kkd.exercise.java.abstracts;
+
+import java.util.AbstractMap;
+
+/**
+ * Abstract class can extends another concrete or abstract Class.
+ */
+public abstract class AbstractClass extends AbstractMap {
+	
+	@Override
+	public Object get(Object key) {
+		return null;
+	}
+}
