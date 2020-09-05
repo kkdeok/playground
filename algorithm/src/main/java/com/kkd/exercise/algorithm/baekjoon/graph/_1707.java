8ef14835b5431ac33dc8fc38e28f6e1a468b58e7
doubleknd26 <doubleknd26@gmail.com>
2020-09-05 16:54:21 +0900

    [algorithm]

--- algorithm/src/main/java/com/kkd/exercise/algorithm/baekjoon/graph/_1707.java
+++ algorithm/src/main/java/com/kkd/exercise/algorithm/baekjoon/graph/_1707.java
@@ -2,66 +2,64 @@ package com.kkd.exercise.algorithm.baekjoon.graph;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.*;
 
 /**
  * https://www.acmicpc.net/problem/1707
  */
 public class _1707 {
 	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	private static final int SIZE = 20001;
-	private static ArrayList<Integer>[] BOARD = new ArrayList[SIZE];
-	private static int[] VISITED = new int[SIZE];
+	private static Map<Integer, List<Integer>> adj = new HashMap<>(); 
+	private static final int[] visit = new int[20001];
 	
 	public static void main(String[] args) throws Exception {
-		String[] line = br.readLine().split(" ");
-		int k = Integer.parseInt(line[0]);
-		
-		while (k-- > 0) {
-			line = br.readLine().split(" ");
-			int n = Integer.parseInt(line[0]);
+		int t = Integer.parseInt(br.readLine());
+		while (t-- > 0) {
+			String[] line = br.readLine().split(" ");
+			int v = Integer.parseInt(line[0]);
 			int e = Integer.parseInt(line[1]);
-
-			for (int i=0 ; i<SIZE ; i++) {
-				BOARD[i] = new ArrayList<>();
-				VISITED[i] = 0;
-			}
 			
-			for (int i = 0; i < e; i++) {
+			for (int i=0 ; i<e ; i++) {
 				line = br.readLine().split(" ");
 				int x = Integer.parseInt(line[0]);
 				int y = Integer.parseInt(line[1]);
-				BOARD[x].add(y);
-				BOARD[y].add(x);
+				
+				List<Integer> listX = adj.getOrDefault(x, new ArrayList<>());
+				listX.add(y);
+				adj.put(x, listX);
+				
+				List<Integer> listY = adj.getOrDefault(y, new ArrayList<>());
+				listY.add(x);
+				adj.put(y, listY);
 			}
+			
 			boolean isBipartiteGraph = true;
-			for (int i=1 ; i<=n ; i++) {
-				if (VISITED[i] == 0) { 
-					isBipartiteGraph = isBipartiteGraph(i, 1);
-					if (!isBipartiteGraph) {
-						break;
-					}
+			for(int i=1 ; i<=v ; i++) {
+				if (visit[i] == 0) {
+					isBipartiteGraph = doDFS(i, 1);
+				}
+				if (!isBipartiteGraph) {
+					break;
 				}
 			}
 			System.out.println(isBipartiteGraph ? "YES" : "NO");
+			
+			Arrays.fill(visit, 0);
+			adj.clear();
 		}
 	}
-
-	private static boolean isBipartiteGraph(int v, int flag) {
-		VISITED[v] = flag;
-		int len = BOARD[v].size();
-		for (int i = 0; i < len; i++) {
-			int nextv = BOARD[v].get(i);
-			
-			if (VISITED[nextv] == 0) {
-				boolean isBipartiteGraph = isBipartiteGraph(nextv, 3 - flag);
-				if (!isBipartiteGraph) {
-					return false;
-				}
-			} else if (VISITED[nextv] == flag) {
-				return false;
+	
+	private static boolean doDFS(int v, int c) {
+		visit[v] = c;
+		boolean ret = true;
+		for (Integer next : adj.getOrDefault(v, new ArrayList<>())) {
+			if (visit[next] == 0) {
+				ret = doDFS(next, 3 - c);
+			} else if (visit[next] == c) {
+				ret = false;
 			}
+			if (!ret) break;
 		}
-		return true;
+		return ret;
 	}
 }
