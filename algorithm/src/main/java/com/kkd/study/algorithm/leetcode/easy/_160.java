c772d5268ba707f100f61fb91a74a10eb6984ff9
kideok <doubleknd26@gmail.com>
2021-09-16 22:03:30 +0900

    algorithm

--- algorithm/src/main/java/com/kkd/study/algorithm/leetcode/easy/_160.java
+++ algorithm/src/main/java/com/kkd/study/algorithm/leetcode/easy/_160.java
@@ -2,39 +2,25 @@ package com.kkd.study.algorithm.leetcode.easy;
 
 import com.kkd.study.algorithm.leetcode.common.ListNode;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class _160 {
-	private Set<ListNode> visit = new HashSet<>();
-
 	public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-		if (headA == null) return null;
-		if (headB == null) return null;
+		traversal(headA, -1);
+		ListNode ret = traversal(headB, 1);
+		traversal(headA, -1);
 
-		if (visit.contains(headA)) return headA;
-		if (visit.contains(headB)) return headB;
-
-		visit.add(headA);
-		visit.add(headB);
-
-		ListNode ret1 = getIntersectionNode(headA.next, headB);
-		if (ret1 != null) return ret1;
-
-		ListNode ret2 = getIntersectionNode(headA, headB.next);
-		if (ret2 != null) return ret2;
-
-		return null;
+		return ret;
 	}
 
-	public static void main(String[] args) {
-		ListNode headA = new ListNode(1);
-		headA.next = new ListNode(4);
+	private ListNode traversal(ListNode node, int checker) {
+		if (node == null) {
+			return node;
+		}
 
-		ListNode headB = new ListNode(2);
+		if (checker > 0 && node.val < 0) {
+			return node;
+		}
 
-
-		_160 program = new _160();
-		program.getIntersectionNode(headA, headB);
+		node.val *= checker;
+		return traversal(node.next, checker);
 	}
 }
