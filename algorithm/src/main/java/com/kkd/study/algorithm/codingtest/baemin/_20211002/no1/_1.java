489522b1a7f900ecc6b5b013e7dd3e337e243f91
kideok <doubleknd26@gmail.com>
2021-10-03 00:24:04 +0900

    baemin algorithm

---
+++ algorithm/src/main/java/com/kkd/study/algorithm/codingtest/baemin/_20211002/no1/_1.java
@@ -0,0 +1,33 @@
+package com.kkd.study.algorithm.codingtest.baemin._20211002.no1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class _1 {
+	public static void main(String[] args) {
+		_1 program = new _1();
+		String result = program.solution("QWERTYUIOASDFGHJKLZXCVBNM");
+		System.out.println(result);
+	}
+
+	public String solution(String message) {
+		String answer = "";
+
+		message = " " + message;
+		char[] tree = message.toCharArray();
+		List<Character> list = new ArrayList<>();
+
+		traversal(list, tree, 1);
+		answer = list.stream().map(String::valueOf).collect(Collectors.joining());
+		return answer;
+	}
+
+	private void traversal(List<Character> list, char[] tree, int key) {
+		if (key <= tree.length -1){
+			traversal(list, tree, key * 2);
+			traversal(list, tree, key * 2 + 1);
+			list.add(tree[key]);
+		}
+	}
+}
---
+++ algorithm/src/main/java/com/kkd/study/algorithm/codingtest/baemin/_20211002/no3/_3.java
@@ -0,0 +1,31 @@
+package com.kkd.study.algorithm.codingtest.baemin._20211002.no3;
+
+import java.util.Arrays;
+
+public class _3 {
+	public static void main(String[] args) {
+		_3 program = new _3();
+		int result = program.solution(new int[][]{{2500, 3}, {700, 5}});
+		System.out.println(result);
+	}
+
+	public int solution(int[][] money) {
+		int answer = -1;
+		Arrays.sort(money, (a, b) -> b[0] - a[0]);
+		int first = 0, second = 0;
+
+		int n = money.length;
+		for (int i=0 ; i<n ; i++) {
+			while (money[i][1] > 0) {
+				if (first > second) {
+					second += money[i][0];
+				} else {
+					first += money[i][0];
+				}
+				money[i][1]--;
+			}
+		}
+		answer = Math.abs(first - second);
+		return answer;
+	}
+}
