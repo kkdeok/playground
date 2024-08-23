fe934e8782d316e80d479e0738d82c8669dcf53d
Kideok <kkdeok26@gmail.com>
2024-08-23 09:49:45 +0900

    gomistakes

--- go/gomistakes/main.go
+++ go/gomistakes/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"github.com/kkdeok/playground/go/gomistakes/variableshadow"
+	"github.com/kkdeok/playground/go/gomistakes/intoverflow"
 )
 
 func main() {
-	variableshadow.Process()
+	intoverflow.Process()
 }
