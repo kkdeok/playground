107c5f2d211c6139a9724daed6f24b172069ede8
Teddy <teddy@daangn.com>
2024-11-06 09:58:13 +0900

    go mistakes

---
+++ go/gomistakes/ch72.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Donation struct {
+	balance int
+	ch      chan int
+}
+
+func main() {
+	donation := &Donation{
+		ch: make(chan int),
+	}
+
+	f := func(goal int) {
+		for balance := range donation.ch {
+			if balance >= goal {
+				fmt.Printf("$%d goal reached\n", balance)
+				return
+			}
+		}
+	}
+
+	go f(10)
+	go f(15)
+
+	for {
+		time.Sleep(time.Second)
+		donation.balance++
+		donation.ch <- donation.balance
+	}
+}
