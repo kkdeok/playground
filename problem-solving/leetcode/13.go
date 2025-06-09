a7c6ecc9c9d96064c6a55da225161d664264a3d8
kideok <kkdeok26@gmail.com>
2025-06-09 09:49:01 -0700

    algorithm

---
+++ problem-solving/leetcode/13.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println(romanToInt("III"))
+}
+
+func romanToInt(s string) int {
+	karr := []string{
+		"M",  // 1000
+		"CM", // 900
+		"D",  // 500
+		"CD", // 400
+		"C",  // 100
+		"XC", // 90
+		"L",  // 50
+		"XL", // 40
+		"X",  // 10
+		"IX", // 9
+		"V",  // 5
+		"IV", // 4
+		"I",  // 1
+	}
+
+	varr := []int{
+		1000,
+		900,
+		500,
+		400,
+		100,
+		90,
+		50,
+		40,
+		10,
+		9,
+		5,
+		4,
+		1,
+	}
+
+	res := 0
+	for len(s) > 0 {
+		for i := 0; i < len(karr); i++ {
+			if strings.HasPrefix(s, karr[i]) {
+				s = strings.TrimPrefix(s, karr[i])
+				res = res + varr[i]
+			}
+		}
+	}
+	return res
+}
---
+++ problem-solving/leetcode/392.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println(isSubsequence("abc", "ahbgdc")) // true
+}
+
+func isSubsequence(s string, t string) bool {
+	if len(s) == 0 {
+		return true
+	}
+	if len(t) == 0 {
+		return false
+	}
+
+	sRightIdx := 0
+	sEndIdx := len(s) - 1
+
+	tStartIndex := 0
+	tEndIndex := len(t) - 1
+}
